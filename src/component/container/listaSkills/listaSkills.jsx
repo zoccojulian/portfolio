8882d8bb3ca942c4cc5a71d583f5da2770e87a77
zoccojulian <zoccojulian@gmail.com>
2023-06-16 10:37:35 -0300

    Comienxo animaciones hover en skills

--- src/component/container/listaSkills/listaSkills.jsx
+++ src/component/container/listaSkills/listaSkills.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './listaSkills.scss';
+import { motion } from 'framer-motion';
 
 const ListaSkills = ({ lista , nombre }) => {
 
@@ -25,11 +26,26 @@ const ListaSkills = ({ lista , nombre }) => {
                 {lista.map((componente, key) =>
                     <li key={key}
                         className='lista__skills-item'
-                        style={{ color: hover == key ? 'red' : 'var(--color__hobbie-selecionado)', transition: 'all .5s' }}
+                        style={{ 
+                            color: hover == key ? 'red' : 'var(--color__hobbie-selecionado)',
+                            transition: 'all .5s'
+                        }}
+                        
                         onMouseOver={() => cambio(key)}
                         onMouseLeave={() => cambio(null)}
                     >
+                        <div 
+                            className='item__background'
+                            style={ { display: hover == key ? 'block' : ' none', animation: 'all 1s'} }
+                        ></div>
                         {componente.icon}
+                        <motion.div
+                            className='item__nombre-container'
+                            animate ={ hover == key ? {transform: 'translateX(0px)', display:'block'} : {transform: 'translateX(-100px)', display:'none'}}
+                            transition={{duration: .5}}
+                        >
+                            <span className='item__nombre'>{ componente.nombre }</span>
+                        </motion.div>
                     </li>
                 )}
             </ul>
