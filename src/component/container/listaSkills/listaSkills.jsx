9b0625066685696611f812f4f43d65b057773e50
zoccojulian <zoccojulian@gmail.com>
2023-06-16 19:18:46 -0300

    Corrección en animacion de skills

--- src/component/container/listaSkills/listaSkills.jsx
+++ src/component/container/listaSkills/listaSkills.jsx
@@ -34,14 +34,22 @@ const ListaSkills = ({ lista , nombre }) => {
                         onMouseOver={() => cambio(key)}
                         onMouseLeave={() => cambio(null)}
                     >
-                        <div 
+                        <motion.div 
                             className='item__background'
-                            style={ { display: hover == key ? 'block' : ' none', animation: 'all 1s'} }
-                        ></div>
-                        {componente.icon}
+                            animate ={ hover == key ? {transform: 'translateY(0px)', visibility:'visible', opacity:1} : {transform: 'translateY(-20px)', visibility:'hidden', opacity:0}}
+                        ></motion.div>
+                        <motion.div 
+                            className='skillIcon__container'
+                            animate ={ hover == key ? {transform: 'translateY(-20px)'} : {transform: 'translateX(0px)'}}
+                            transition={{duration: .5}}
+                        >
+                        
+                            {componente.icon}
+                        </motion.div>
                         <motion.div
+                            layout
                             className='item__nombre-container'
-                            animate ={ hover == key ? {transform: 'translateX(0px)', display:'block'} : {transform: 'translateX(-100px)', display:'none'}}
+                            animate ={ hover == key ? {transform: 'translateX(0px)', visibility:'visible'} : {transform: 'translateX(50px)', visibility:'hidden'}}
                             transition={{duration: .5}}
                         >
                             <span className='item__nombre'>{ componente.nombre }</span>
