1d4a8badd147a6142765b09c22b3baaf4765e91a
zoccojulian <zoccojulian@gmail.com>
2023-07-05 11:04:19 -0300

    Prueba correcion de fotos para rendimiento

--- src/component/container/secciones/contacto/contacto.jsx
+++ src/component/container/secciones/contacto/contacto.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './contacto.scss';
 import useIsInView from '../../../hooks/useIsInView';
 import { secciones } from '../../../../js/secciones';
-import imagen from '../../../../assets/foto/foto_02_c.png'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
--- src/component/pure/liFormacion/liFormacion.jsx
+++ src/component/pure/liFormacion/liFormacion.jsx
@@ -8,12 +8,15 @@ const LiFormacion = ( { formacion } ) => {
     return (
         <li className='liFormacion'>
             <div className='liFormacion__foto-container'>
-                <img src={formacion.foto}></img>
+                <img 
+                    src={formacion.foto}
+                    alt= { formacion.foto.match(/\/([^/]+)\.\w+$/)[1] }
+                ></img>
             </div>
             <div className='liFormacion__texto-container'>
-                <h4 className='liformacion__texto-titulo liFormacion__textos'>{ formacion.titulo }</h4>
-                <h5 className='liformacion__texto-institucion liFormacion__textos'>{ formacion.institucion }</h5>
-                <h5 className='liformacion__texto-periodo liFormacion__textos'>{ formacion.periodo }</h5>
+                <h3 className='liformacion__texto-titulo liFormacion__textos'>{ formacion.titulo }</h3>
+                <h4 className='liformacion__texto-institucion liFormacion__textos'>{ formacion.institucion }</h4>
+                <h4 className='liformacion__texto-periodo liFormacion__textos'>{ formacion.periodo }</h4>
             </div>
         </li>
     );
